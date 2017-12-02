7db227413ccab41f37b5ec5b45b4235628f1e356
Paul Robins <lolhi@butt.es>
2017-12-02 13:56:13 +0000

    Tidy up some warts in Part 2

--- holycrap/lib/Day2.pm
+++ holycrap/lib/Day2.pm
@@ -1,15 +1,14 @@
 unit module Day2;
 
-# %% returns a bool, it'd be nicer if it returned the division, so do that
-sub infix:<%/%> { $^l/$^r if $^l %% $^r }
-sub evenly-divisible(@c ($l, $r)) { $l %/% $r or $r %/% $l }
+# Due to sorting the list in part2, l ≤ r so we need only one test
+sub evenly-divisible(@c ($l, $r)) { $r/$l if $r%%$l }
 
 # I'd rather these were in the .p6 but I need to use them from tests too
 sub part1 is export {$^l.max - $^l.min}
-sub part2 is export {[+] $^l.combinations(2).map: &evenly-divisible}
+sub part2 is export {[+] $^l.sort.combinations(2).map: &evenly-divisible}
 
 # Call a Callable on each line after splitting them into a Numeric list
 sub process-file(Str $f where *.IO.r, Callable $c) is export {
-	$f.IO.lines».split(/\t/)».Numeric.map: $c;
+	$f.IO.lines».words».Numeric.map: $c;
 }
 
