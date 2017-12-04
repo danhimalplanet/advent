7a5775d368ccd5d53a0c81bd1a53743b7bd79358
Devin Austin <devin.austin@gmail.com>
2017-12-04 14:47:16 -0700

    initial day 3 commit

---
+++ leonard_rockstein/day3-manhatten-distance/src/main/java/com/advent2017/ManhattenDistance.java
@@ -0,0 +1,14 @@
+package com.advent2017;
+
+
+
+public class ManhattenDistance {
+
+    private static final Integer input = 347991;
+    public static void main(String[] args) {
+        System.out.println("INPUT " + input);
+
+    }
+
+
+}
