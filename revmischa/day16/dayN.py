d25bbc5fbc33af318dceb3e7aac81c8f8cd669ac
Mischa Spiegelmock <revmischa@cpan.org>
2017-12-17 06:38:07 -0800

    day 16 part B - wrong but no idea why

--- revmischa/day16/dayN.py
+++ revmischa/day16/dayN.py
@@ -18,24 +18,38 @@ class DayN(Computer):
         super().__init__(structure)
         self.input = structure
         self.moves = moves
-        chars = [chr(x) for x in range(ord('a'), ord('a') + size)]
+        chars = [x for x in range(0, size)]
         self.l_ = np.asarray(chars)
+        self.insts = []
+        self.parse_instructions()
+
+        self.seen = {}
 
     @classmethod
     def parse_input(cls, input_str: str):
         insts = input_str.split(',')
         return insts
 
+    def parse_instructions(self):
+        for inst in self.input:
+            if inst.startswith('s'):
+                spin = int(inst[1:])
+                self.insts.append((self.spin, spin))
+            elif inst.startswith('x'):
+                swap_a, swap_b = inst[1:].split('/')
+                self.insts.append((self.swap_idx, int(swap_a), int(swap_b)))
+            elif inst.startswith('p'):
+                a, b = inst[1:].split('/')
+                a = ord(a) - ord('a')
+                b = ord(b) - ord('a')
+                self.insts.append((self.swap_ele, a, b))
+            else:
+                raise Exception(f"unknown instruction {inst}")
+
     def do_inst(self, inst):
-        if inst.startswith('s'):
-            spin = int(inst[1:])
-            self.spin(spin)
-        elif inst.startswith('x'):
-            swap_a, swap_b = inst[1:].split('/')
-            self.swap_idx(int(swap_a), int(swap_b))
-        elif inst.startswith('p'):
-            a, b = inst[1:].split('/')
-            self.swap_ele(a, b)
+        func = inst[0]
+        args = inst[1:]
+        func(*args)
 
     def spin(self, n):
         self.l_ = np.roll(self.l_, n)
@@ -54,19 +68,30 @@ class DayN(Computer):
     def __repr__(self):
         r = ""
         for i in self.l_:
-            r += str(i)
+            r += chr(i + ord('a'))
         return r
 
     def run_part1(self):
-        for inst in self.input:
+        for inst in self.insts:
             self.do_inst(inst)
         return str(self)
 
     def run_part2(self):
-        for _ in range(0, self.moves):
-            for inst in self.input:
+        sl = []
+        for n in range(0, self.moves):
+            # print(f"Move {n}")
+            for inst in self.insts:
                 self.do_inst(inst)
-        return str(self)
+
+            s = str(self)
+            sl.append(s)
+
+            if s in self.seen:
+                # print(f"Found cycle at iter {n} {s}: {self.seen[s]} cycles: {cycle_num}, n%c {self.moves % cycle_count}")
+                return sl[self.moves % n]
+            else:
+                self.seen[s] = 1
 
 if __name__ == '__main__':
     main(DayN, moves=1000000000)
+    #main(DayN, moves=100)
