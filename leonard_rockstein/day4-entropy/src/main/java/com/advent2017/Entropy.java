6407df289a15303b3269b6b8fab05ab871c636d6
Devin Austin <devin.austin@gmail.com>
2017-12-16 18:13:37 -0700

    i have no fucking clue why this isn't working. I get the correct results when I use the test cases from the problem that I've put in passphrases2.txt

--- leonard_rockstein/day4-entropy/src/main/java/com/advent2017/Entropy.java
+++ leonard_rockstein/day4-entropy/src/main/java/com/advent2017/Entropy.java
@@ -3,6 +3,7 @@ package com.advent2017;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
@@ -12,43 +13,79 @@ import java.util.List;
 import java.util.Map;
 
 public class Entropy {
-
-
+    
+    
     public static void main(String[] args) throws IOException {
         Entropy entropy = new Entropy();
-        System.out.println("Number of valid passphrases: " + entropy.numberOfValidPassphrases());
-
+        System.out.println("Number of valid passphrases: " + entropy.numberOfNonRepeatingPassphrases());
+        System.out.println("Number of valid non anagram passphrases: " + entropy.numberOfValidPassphrases());
+        
     }
-
+    
     public int numberOfValidPassphrases() throws IOException {
-        int validPassphrases = 0;
-        for (String line : this.readTsv("/passphrases.txt")) {
-            if (validPassphrase(line)) {
-                validPassphrases += 1;
+        List<String> validPassphrases = new ArrayList<>();
+        for (String line : this.nonRepeatingPassphrases()) {
+            if (!this.containsAnagram(line)) {
+                validPassphrases.add(line);
             }
         }
-        return validPassphrases;
+        
+        return validPassphrases.size();
     }
-
-    public boolean validPassphrase(String passphrase) {
+    
+    public List<String> nonRepeatingPassphrases() throws IOException {
+        List<String> nonRepeating = new ArrayList<>();
+        for (String line : this.readFile("/passphrases.txt")) {
+            if (this.nonRepeatingPassphrase(line)) {
+                nonRepeating.add(line);
+            }
+        }
+        return nonRepeating;
+    }
+    
+    public int numberOfNonRepeatingPassphrases() throws IOException {
+        return this.nonRepeatingPassphrases().size();
+    }
+    
+    public boolean containsAnagram(String line) {
+        Map<String, String> anagramHashes = new HashMap<>();
+        String[] wordsInLine = line.split("\\s");
+        String baseWord = wordsInLine[0].toLowerCase();
+        anagramHashes.put(hashAnagram(baseWord), baseWord);
+        for (String comparisonWord : Arrays.copyOfRange(wordsInLine, 1, wordsInLine.length)) {
+            if (anagramHashes.containsKey(hashAnagram(comparisonWord.toLowerCase()))) {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+    
+    public String hashAnagram(String word) {
+        char[] characters = word.toCharArray();
+        Arrays.sort(characters);
+        return new String(characters);
+    }
+    
+    public boolean nonRepeatingPassphrase(String passphrase) {
         Map<String, Boolean> seen = new HashMap<>();
-
+        
         String[] words = passphrase.split("\\s");
-
+        
         for (String word : Arrays.asList(words)) {
             if (seen.containsKey(word)) {
                 return false;
             }
             seen.put(word, true);
         }
-
+        
         return true;
-
+        
     }
-
-    private List<String> readTsv(String fileName) throws IOException {
+    
+    private List<String> readFile(String fileName) throws IOException {
         List<String> lines = new ArrayList<>();
-
+        
         InputStream is = Entropy.class.getResourceAsStream(fileName);
         BufferedReader reader = new BufferedReader(new InputStreamReader(is));
         String line;
@@ -56,7 +93,7 @@ public class Entropy {
             lines.add(line);
         }
         return lines;
-
+        
     }
-
+    
 }
