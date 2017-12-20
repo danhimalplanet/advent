f472afddb9bab8620ea6f3537885f24c30eff5d2
Evan Dierlam <evan.dierlam@aenetworks.com>
2017-12-20 17:41:36 -0500

    updates to structures

--- vor/day09/day09a.tcl
+++ vor/day09/day09a.tcl
@@ -3,15 +3,22 @@ set fp [open "day09.txt" r]
 set stream [read $fp $fsize]
 close $fp
 
+set layer 1
 regsub -all {!!|!>} $stream {} swap
 regsub -all {<(?:[^>]+)?>|,} $swap {} swap
 puts $swap
 
-for {set x 1}{$x != $x} {incr x} {
+regsub -all {\{\}} $swap {1,} swap
+puts $swap
+
+while 1 {
+
 # if ({([\d,]+)})
 #   find all (?<={)([\d,]+)(?=}) and increase each number by 1
 #   find all ({([\d,]+)}) and replace with 1,$2
 # else break
+	break
 }
+
 # sum all ,
 # puts $total
