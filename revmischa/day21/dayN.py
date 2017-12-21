d8d0853af8842bccbb8869d4dbda70ac98af8730
Mischa Spiegelmock <revmischa@cpan.org>
2017-12-21 08:36:41 -0800

    speed enhancements of dubious effectiveness

--- revmischa/day21/dayN.py
+++ revmischa/day21/dayN.py
@@ -7,8 +7,9 @@ if PROJ_ROOT not in sys.path:
 
 from revmischa import Computer, main
 from pprint import pprint
-import numpy as np
-from typing import List, Tuple
+from typing import List, Tuple, Dict
+from functools import partial
+from concurrent.futures import ThreadPoolExecutor
 
 
 Pattern = List[str]
@@ -23,9 +24,20 @@ class DayN(Computer):
         """Construct solver with puzzle input."""
         super().__init__(structure)
         self.rules: RuleList = structure
+        self.rules_2: RuleList = []
+        self.rules_3: RuleList = []
+        for rule in self.rules:
+            if len(rule[0]) == 2:
+                self.rules_2.append(rule)
+            elif len(rule[0]) == 3:
+                self.rules_3.append(rule)
+            else:
+                raise Exception("you f'd up")
         self.iterations = iterations
         self.size = 3
         self.pattern: Pattern = ['.#.', '..#', '###']
+        self.executor = ThreadPoolExecutor(max_workers=100)
+        self.pattern_cache: Dict[str] = {}
 
     @classmethod
     def parse_input(cls, input_str: str) -> RuleList:
@@ -33,9 +45,18 @@ class DayN(Computer):
         rules: RuleList = list()
         for r in rules_str:
             i, o = r.split(' => ')
-            i_rows = i.split('/')
+            rule_in = i.split('/')
             o_rows = o.split('/')
-            rules.append((i_rows, o_rows))
+
+            # a smart thing to do here would be to pre-flip/rotate the rules for faster matching later
+            rule_matches = [rule_in, cls.flip_x(rule_in), cls.flip_y(rule_in)]
+            rule_matches.append(cls.flip_x(cls.rotate(rule_in)))
+            rule_matches.append(cls.flip_y(cls.rotate(rule_in)))
+            rule_matches.append(cls.rotate(rule_in))
+            rule_matches.append(cls.rotate(cls.rotate(cls.rotate(rule_in))))
+            for rule_transformed in rule_matches:
+                rules.append((rule_transformed, o_rows))
+
         return rules
 
     def subdivide(self) -> Pattern:
@@ -54,54 +75,79 @@ class DayN(Computer):
         new_pattern: Pattern = []
 
         # divide into nxn segments
-        new_row_idx = 0
-        new_square_size = None
+        sub_patterns: List[Pattern] = []  # subdivided squares
         for i in range(0, self.size, divisor):
             # get n rows
             sub_pattern_rows = self.pattern[i:i+divisor]
-            print(f"r: {sub_pattern_rows}")
+            # print(f"r: {sub_pattern_rows}")
             # get n cols at a time
             sub_pattern: Pattern = []
             for j in range(0, self.size, divisor):
                 sub_pattern_cols = [row[j:j+divisor] for row in sub_pattern_rows]
-                print(f"[{j}:{j}+{divisor}]sub cols: {sub_pattern_cols}")
-                sub_pattern = self.apply_rules(sub_pattern_cols, divisor)
-
-                # update new pattern
-                print("new sub pattern:")
-                print(sub_pattern)
-                new_square_size = len(sub_pattern)
-                for r_i in range(0, new_square_size):
-                    r_i_ = r_i + new_row_idx
-                    print(f"r_i_: {r_i_}")
-                    if r_i_ not in new_pattern:
-                        new_pattern.append('')
-                    new_pattern[r_i_] += sub_pattern[r_i]
-                new_pattern_squares.append(sub_pattern)
-
-            # increase rows
-            new_row_idx += new_square_size
+                sub_patterns.append(sub_pattern_cols)
             new_size += divisor + 1
-        print("new pattern squares:")
-        print(new_pattern_squares)
+
+        # rule match / transform
+        if divisor == 2:
+            rules = self.rules_2
+        elif divisor == 3:
+            rules = self.rules_3
+        else:
+            raise Exception("you f'd up")
+        apply_partial = partial(self.apply_rules, divisor=divisor, rules=rules)
+
+        new_squares: List[Pattern] = []
+        # option I: threaded
+        # new_squares = list(self.executor.map(apply_partial, sub_patterns))
+        # option II: not threaded
+        for sub_pattern_cols in sub_patterns:
+            sub_pattern = apply_partial(sub_pattern_cols)
+            new_squares.append(sub_pattern)
+
+        # stitch together
+        i = 0
+        new_row_idx = 0
+        new_square_size = None
+        for sub_pattern in new_squares:
+            new_square_size = len(sub_pattern)
+            # print(f"new square size: {new_square_size}")
+            for r_i in range(0, new_square_size):
+                r_i_ = r_i + new_row_idx
+                # print(f"r_i_: {r_i_}")
+                if r_i_ not in new_pattern:
+                    new_pattern.append('')
+                    new_pattern[r_i_] += sub_pattern[r_i]
+                    new_pattern_squares.append(sub_pattern)
+
+                i += 1
+                # increase rows
+                if i % new_size == 0:
+                    # print(f"new row increase @ {i}")
+                    new_row_idx += new_square_size
+
+        # print("new pattern squares:")
+        # print(new_pattern_squares)
         new_pattern = new_pattern[0:new_size]
-        print("new pattern: ")
-        print(new_pattern)
+        # print("new pattern: ")
+        # print(new_pattern)
 
         new_divisor = 3 if divisor == 2 else 3
         self.size = new_size
         print(f"new size: {new_size}, new pattern:")
-        print(new_pattern)
+        # print(new_pattern)
 
         return new_pattern
 
-    def flip_x(self, p: Pattern) -> Pattern:
+    @classmethod
+    def flip_x(cls, p: Pattern) -> Pattern:
         return list(reversed(p))
 
-    def flip_y(self, p: Pattern) -> Pattern:
+    @classmethod
+    def flip_y(cls, p: Pattern) -> Pattern:
         return [''.join(reversed(row)) for row in p]
 
-    def rotate(self, p: Pattern) -> Pattern:
+    @classmethod
+    def rotate(cls, p: Pattern) -> Pattern:
         new_rows: Pattern = []
         matrix: List[List[str]] = []
         for row in p:
@@ -113,37 +159,18 @@ class DayN(Computer):
             new_rows.append(''.join(row))
         return new_rows
 
-    def apply_rules(self, square: Pattern, divisor: int):
+    def apply_rules(self, square: Pattern, divisor: int, rules: RuleList):
         square_str = '\n'.join(square)
-        for rule in self.rules:
+        if square_str in self.pattern_cache:
+            return self.pattern_cache[square_str]
+        for rule in rules:
             rule_in, rule_out = rule
-            # rule is the wrong size
-            desired_rule_len = divisor
-            # print(f"desired rule len: {desired_rule_len}, rule in len: {len(rule_in[0])}")
-            # print("matching:")
-            # print(square_str)
-            if len(rule_in[0]) != desired_rule_len:
-                continue
-
-            rule_matches = [rule_in, self.flip_x(rule_in), self.flip_y(rule_in)]
-            rule_matches.append(self.flip_x(self.rotate(rule_in)))
-            rule_matches.append(self.flip_y(self.rotate(rule_in)))
-            rule_matches.append(self.rotate(rule_in))
-            rule_matches.append(self.rotate(self.rotate(self.rotate(rule_in))))
-
-            for rci in rule_matches:
-                # print(f"RC => {rco}")
-                # print(rci)
-                rc_str = '\n'.join(rci)
-                if rc_str == square_str:
-                    # matched
-                    # print(f"matched rule!")
-                    # print(rc_str)
-                    # print(square)
-                    return rule_out
+            if rule_in == square:
+                self.pattern_cache[square_str] = rule_out
+                return rule_out
         print("no match:")
         print(square_str)
-        raise Exception()
+        raise Exception("i suck")
 
         return square
 
@@ -162,4 +189,5 @@ class DayN(Computer):
         return 0
 
 if __name__ == '__main__':
-    main(DayN, iterations=5)
+    print(DayN.part1_result(iterations=5))
+    print(DayN.part1_result(iterations=13))
