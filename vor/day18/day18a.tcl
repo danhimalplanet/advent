531b60cbc5f678becbbbc1c0da84edf594ffe733
Evan Dierlam <evan@debuggered.com>
2017-12-24 11:45:47 -0800

    day 18a

--- vor/day18/day18a.tcl
+++ vor/day18/day18a.tcl
@@ -3,11 +3,11 @@ set fp [open "day18.txt" r]
 set stream [read $fp $fsize]
 close $fp
 
-array set notes {a 0 b 0 f 0 i 0 p 0 as 0 bs 0 fs 0 is 0 ps 0}
-
 set stream [split $stream '\n']
-puts "======================"
-for {set x 0} {$x <  [llength $stream]} {incr x} {  
+array set notes {a 0 b 0 f 0 i 0 p 0 as 0 bs 0 fs 0 is 0 ps 0}
+set key {a b f i p}
+
+for {set x 0} {$x <  [llength $stream] } {incr x} {  
 
   set line [lindex $stream $x]
   set elements [split $line " "]
@@ -17,42 +17,39 @@ for {set x 0} {$x <  [llength $stream]} {incr x} {
   set note [concat [expr {$target}]s]
 
   if {[regexp {[a-z]+} $value] == 1} {
-    puts "Val: $value"
     set value $notes($value)
-    puts "Val set to $value"
-    #clarify this.
+  } 
+
+  if {[regexp {[0-9]+} $target]} {
+    set target [lindex $key $target]
+    set note [concat [expr {$target}]s]
   }
-  puts "For: $target Notes: $notes($target) Value: $value on level $x"
+  
   switch $cmd {
     snd {
-      puts "send $value to $notes($note) from $note"
-      set notes($note) $value
+      set notes($note) $notes($target)
     }
     set {
-      puts "set $target to $value"
       set notes($target) $value
     }
     add {
-      puts "add notes($target) to $value"
       set notes($target) [expr {$notes($target) + $value}]
     }
     mul {
-      puts "multiplied $value by $notes($target)"
       set notes($target) [expr {$notes($target) * $value}]
     }
     mod {
-      puts "mod set notes($target) to [expr {$notes($target) % $value}]"
       set notes($target) [expr {$notes($target) % $value}]
     }
     rcv {
       if {$notes($note) != 0} {
-      puts "rcv notes($target) to $notes($note) from $note"
-        set notes($target) $notes($note)
+      puts "$notes($note)"
+      break
       }
     }
     jgz {
-      if {$notes($target) > 0} {
-        incr x $value
+      if {$notes($target) != 0} {
+        incr x [expr {$value - 1}]
       }
     }
   }
