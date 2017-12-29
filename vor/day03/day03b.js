50f72c3bd8702c79ded89e6e9c74b4968297b6de
Evan Dierlam <evan@debuggered.com>
2017-12-29 00:39:31 -0500

    day 3 cleanup

--- vor/day03/day03b.js
+++ vor/day03/day03b.js
@@ -1,87 +1,89 @@
-let input = 289326
-let grid = {}
+(function(){
+    let input = 289326
+    let grid = {}
 
-let total = 0
-let layer = 1
-let center = 5
-let direction = 'right'
-let x = center
-let y = center
-let current = (x * 10) + y
-grid[current] = 1
+    let total = 0
+    let layer = 1
+    let center = 5
+    let direction = 'right'
+    let x = center
+    let y = center
+    let current = (x * 10) + y
+    grid[current] = 1
 
-while (total <= input ) {
-    switch (direction) {
-        case 'right': 
-            if (x < (center + layer)) {
-                x += 1
-            } else {
-                direction = 'up'
-                y += 1
-            }
-            break
-        case 'up':
-            if (y < (center + layer)) {
-                y += 1
-            } else {
-                direction = 'left'
-                x -= 1
-            }
-            break
-        case 'left':
-            if (x > (center - layer)) {
-                x -= 1
-            } else {
-                direction = 'down'
-                y -= 1
-            }
-            break
-        case 'down':
-            if (y > (center - layer)) {
-                y -= 1
-            } else {
-                direction = 'right'
-                x += 1
-                layer += 1
-            }       
-            break
+    while (total <= input ) {
+        switch (direction) {
+            case 'right': 
+                if (x < (center + layer)) {
+                    x += 1
+                } else {
+                    direction = 'up'
+                    y += 1
+                }
+                break
+            case 'up':
+                if (y < (center + layer)) {
+                    y += 1
+                } else {
+                    direction = 'left'
+                    x -= 1
+                }
+                break
+            case 'left':
+                if (x > (center - layer)) {
+                    x -= 1
+                } else {
+                    direction = 'down'
+                    y -= 1
+                }
+                break
+            case 'down':
+                if (y > (center - layer)) {
+                    y -= 1
+                } else {
+                    direction = 'right'
+                    x += 1
+                    layer += 1
+                }       
+                break
+        }
+
+        current = (x * 10) + y
+        total = 0
+
+        // Right
+        tindex = ((x + 1) * 10) + y
+        total += (grid[tindex] == undefined)? 0 : grid[tindex]
+
+        // Top Right
+        tindex = ((x + 1) * 10) + (y + 1)
+        total += (grid[tindex] == undefined)? 0 : grid[tindex]
+
+        // Top
+        tindex = (x * 10) + (y + 1)
+        total += (grid[tindex] == undefined)? 0 : grid[tindex]
+
+        // Top Left
+        tindex = ((x - 1) * 10) + (y + 1)
+        total += (grid[tindex] == undefined)? 0 : grid[tindex]
+
+        // Left
+        tindex = ((x - 1) * 10) + y
+        total += (grid[tindex] == undefined)? 0 : grid[tindex]
+
+        // Bottom Left
+        tindex = ((x - 1) * 10) + (y - 1)
+        total += (grid[tindex] == undefined)? 0 : grid[tindex]
+
+        // Bottom
+        tindex = (x * 10) + (y - 1)
+        total += (grid[tindex] == undefined)? 0 : grid[tindex]
+
+        // Bottom Right
+        tindex = ((x + 1) * 10) + (y - 1)
+        total += (grid[tindex] == undefined)? 0 : grid[tindex]
+
+        grid[current] = total 
     }
-
-    current = (x * 10) + y
-    total = 0
-
-    // Right
-    tindex = ((x + 1) * 10) + y
-    total += (grid[tindex] == undefined)? 0 : grid[tindex]
-
-    // Top Right
-    tindex = ((x + 1) * 10) + (y + 1)
-    total += (grid[tindex] == undefined)? 0 : grid[tindex]
-
-    // Top
-    tindex = (x * 10) + (y + 1)
-    total += (grid[tindex] == undefined)? 0 : grid[tindex]
-
-    // Top Left
-    tindex = ((x - 1) * 10) + (y + 1)
-    total += (grid[tindex] == undefined)? 0 : grid[tindex]
-
-    // Left
-    tindex = ((x - 1) * 10) + y
-    total += (grid[tindex] == undefined)? 0 : grid[tindex]
-
-    // Bottom Left
-    tindex = ((x - 1) * 10) + (y - 1)
-    total += (grid[tindex] == undefined)? 0 : grid[tindex]
-
-    // Bottom
-    tindex = (x * 10) + (y - 1)
-    total += (grid[tindex] == undefined)? 0 : grid[tindex]
-
-    // Bottom Right
-    tindex = ((x + 1) * 10) + (y - 1)
-    total += (grid[tindex] == undefined)? 0 : grid[tindex]
-
-    grid[current] = total 
-}
-console.log(total)
+    console.log(total)
+})()
