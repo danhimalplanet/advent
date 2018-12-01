af11a4a8898708610a10721adbfad4a2074f9b83
Devin Austin <devin.austin@gmail.com>
2018-12-01 11:02:46 -0700

    day 1 part 1 complete

--- 2018/dja/day1/src/main/scala/io/dja/advent/day1/Run.scala
+++ 2018/dja/day1/src/main/scala/io/dja/advent/day1/Run.scala
@@ -5,9 +5,13 @@ import scala.io.Source
 object Run {
   def main(args: Array[String]): Unit = {
     val lines: Iterator[String] = Source.fromResource("input").getLines
+    var result: Int = 0
+
     while (lines.hasNext) {
-      println(lines.next)
+      val next = lines.next.replace("+", "")
+      result += next.toInt
     }
+    println(s"result: ${result}")
   }
 }
 
