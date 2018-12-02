c8389310b36f71a24184ee977dc0b237c92b38c1
Devin Austin <devin.austin@gmail.com>
2018-12-02 14:31:28 -0700

    part 1 done

--- 2018/dja/day2/src/main/scala/io/dja/advent/day2/Run.scala
+++ 2018/dja/day2/src/main/scala/io/dja/advent/day2/Run.scala
@@ -1,16 +1,44 @@
 package io.dja.advent.day2
 
+
+import scala.collection.immutable.HashMap
 import scala.io.Source
 
 object Run extends App {
 
   val data = loadData(args(0))
-  println(s"DATA ${data}")
+  var seen: HashMap[String, Int] = new HashMap[String, Int]()
+
+  for (line <- data) {
+    line.toCharArray.foreach { character =>
+      var threes: Int = 0
+      var twos: Int = 0
+      line.count(_ == character) match {
+        case 1 => println(s"skipping")
+        case 2 => markComplete(2, line)
+        case 3 => markComplete(3, line)
+      }
+
+    }
+  }
+
+  var twos: Int = 0
+  var threes: Int = 0
+  seen.foreach { case (k, v) =>
+    v match {
+      case 2 => twos += 1
+      case 3 => threes += 1
+    }
+
+  }
+  println(s"Part 1 result: ${twos * threes}")
+
+  def markComplete(count: Int, line: String): Unit = {
+    seen += (s"${line}${count}" -> count)
+  }
   def loadData(file: String): List[String] = {
     Source.fromFile(file)
       .getLines()
       .toList
   }
-
-
 }
