2ff614a03def0c9c9bc42567e59ec670f89ced6a
Devin Austin <devin.austin@gmail.com>
2018-12-02 13:13:57 -0700

    fixed naming, basic file loading

--- 2018/dja/day2/src/main/scala/io/dja/advent/day2/Run.scala
+++ 2018/dja/day2/src/main/scala/io/dja/advent/day2/Run.scala
@@ -1,6 +1,16 @@
 package io.dja.advent.day2
 
+import scala.io.Source
+
 object Run extends App {
+
+  val data = loadData(args(0))
+  println(s"DATA ${data}")
+  def loadData(file: String): List[String] = {
+    Source.fromFile(file)
+      .getLines()
+      .toList
+  }
 
 
 }
