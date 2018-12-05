da9b3f940e48e9d67cdcf484398f29bc0fd3f9c9
Jordan <jordan@eremieff.com>
2018-12-05 22:45:12 +1100

    Day 5 part b 2 ez

--- 2018/erm/day05/day5.py
+++ 2018/erm/day05/day5.py
@@ -1,26 +1,38 @@
+def get_result(line) -> list:
+    res = []
+    for i in line:
+        if res:
+            if res[-1] != i.swapcase():
+                res.append(i)
+            else:
+                res.pop()
+        else:
+            res.append(i)
+
+    return res
+
+
 def main() -> None:
 
     with open("data.txt", "r") as f:
         input_data = [s.strip() for s in f.readlines()]
 
     result_a = []
+    result_b = 0
+    results = {}
 
     for line in input_data:
-        res = []
-        for i in line:
-            if res:
-                if res[-1] != i.swapcase():
-                    res.append(i)
-                else:
-                    res.pop()
-            else:
-                res.append(i)
-
+        res = get_result(line)
         result_a.extend(res)
+        for i in set(line.lower()):
+            _line = line.replace(i, "").replace(i.upper(), "")
+            results[i] = len(get_result(_line))
 
     result_a = len(result_a)
+    result_b = min(results.values())
 
     print(f"Part A: {result_a}")
+    print(f"Part B: {result_b}")
 
 
 if __name__ == "__main__":
