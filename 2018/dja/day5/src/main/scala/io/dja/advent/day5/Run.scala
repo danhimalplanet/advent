9bc2360c5990bf8818b41d199a8a40f1941c213c
Devin Austin <c-devin.austin@charter.com>
2018-12-05 16:35:54 -0700

    cheated off snb to get the loop breaking correct for part 1

--- 2018/dja/day5/src/main/scala/io/dja/advent/day5/Run.scala
+++ 2018/dja/day5/src/main/scala/io/dja/advent/day5/Run.scala
@@ -3,33 +3,37 @@ package io.dja.advent.day5
 import scala.collection.mutable.ArrayBuffer
 import scala.io.Source
 import scala.util.control.Breaks._
+
 object Run extends App {
 
   var data = loadData(args(0))
   var stackFromData: ArrayBuffer[Char] = data.flatten.toArray.to[ArrayBuffer]
-  var keep: ArrayBuffer[Char] = new ArrayBuffer[Char]()
 
   var i = 0
+  var changesDetected = false
+  var finished = false
   breakable {
-    while (i < stackFromData.size) {
-      val next = i+1
-      if (next >= stackFromData.size) {
-        break
-      }
+    while (!finished) {
+      if (stackFromData.size == 0) break
+      val next = i + 1
       val a = stackFromData(i)
       val b = stackFromData(next)
-      if (a == '1') {
-        stackFromData.remove(i)
-        i = 0
-      }
-      if (a != '1' && b != '1') {
-        if (a == b.toLower || b == a.toLower) {
-          stackFromData(i) = '1'
-          stackFromData(next) = '1'
-          i = 0
+      if (a == b.toLower || b == a.toLower) {
+        if (a.isLower && b.isUpper || b.isLower && a.isUpper) {
+          stackFromData.remove(i)
+          stackFromData.remove(i)
+          changesDetected = true
+          i -= 1
         }
       }
-      i += 1
+      if (next >= stackFromData.size - 1 && changesDetected == false) {
+        break
+      } else if (next >= stackFromData.size - 1 && changesDetected == true) {
+        i = 0
+        changesDetected = false
+      } else {
+        i += 1
+      }
     }
   }
 
