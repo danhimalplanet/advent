56f3c085ed807e51041c56939dc6841da5fbed66
Steven Bloch <snbloch@gmail.com>
2019-12-01 22:24:42 -0800

    day02, JS

---
+++ 2019/snbloch/day02/02b.js
@@ -0,0 +1,40 @@
+var puzzleinput = [1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,1,10,19,1,6,19,23,1,13,23,27,1,6,27,31,1,31,10,35,1,35,6,39,1,39,13,43,2,10,43,47,1,47,6,51,2,6,51,55,1,5,55,59,2,13,59,63,2,63,9,67,1,5,67,71,2,13,71,75,1,75,5,79,1,10,79,83,2,6,83,87,2,13,87,91,1,9,91,95,1,9,95,99,2,99,9,103,1,5,103,107,2,9,107,111,1,5,111,115,1,115,2,119,1,9,119,0,99,2,0,14,0];
+var input = [];
+
+function computeOperation (position) {
+    if (input[position] == 99) {
+        return -1;
+    }
+    else if (input[position] == 1) {
+        input[input[position + 3]] = input[input[position + 1]] + input[input[position + 2]];
+        return (position + 4);
+    }
+    else if (input[position] == 2) {
+        input[input[position + 3]] = input[input[position + 1]] * input[input[position + 2]];
+        return (position + 4);
+    }
+
+}
+
+function main() {
+    input = puzzleinput.slice(0);
+    for (var i = 0; i < 100; i++) {
+        for (var j = 0; j < 100; j++) {
+            position = 0;
+            input[1] = i;
+            input[2] = j;
+            while (position != -1) {
+                position = computeOperation(position);
+            }
+            if (input[0] == 19690720) {
+                console.log(100 * i + j);
+                return;
+            }
+            else {
+                input = puzzleinput.slice(0);
+            }
+        }
+    }
+}
+
+main();
