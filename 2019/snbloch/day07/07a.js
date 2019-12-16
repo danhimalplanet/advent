3182519a7240ab160b912dee0a9f7acf527c65e6
Steven Bloch <snbloch@gmail.com>
2019-12-15 17:07:03 -0800

    Refactor day07 first part to use class-based intcode computer.

--- 2019/snbloch/day07/07a.js
+++ 2019/snbloch/day07/07a.js
@@ -1,120 +1,126 @@
 var Combinatorics = require('js-combinatorics');
 
 var puzzleInput = [3,8,1001,8,10,8,105,1,0,0,21,42,67,88,101,114,195,276,357,438,99999,3,9,101,3,9,9,1002,9,4,9,1001,9,5,9,102,4,9,9,4,9,99,3,9,1001,9,3,9,1002,9,2,9,101,2,9,9,102,2,9,9,1001,9,5,9,4,9,99,3,9,102,4,9,9,1001,9,3,9,102,4,9,9,101,4,9,9,4,9,99,3,9,101,2,9,9,1002,9,3,9,4,9,99,3,9,101,4,9,9,1002,9,5,9,4,9,99,3,9,102,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,1,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,1,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,99,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,1,9,9,4,9,99,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,1,9,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,1,9,4,9,99,3,9,102,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,1,9,9,4,9,3,9,101,1,9,9,4,9,3,9,101,1,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,99,3,9,1001,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,101,2,9,9,4,9,99];
-var diagnostic = [];
-var inputInstruction = [];
-var output = [];
-var currentInstruction;
-var firstParameterMode;
-var secondParameterMode;
-var thirdParameterMode;
 
-function computeParameterModes (instruction) {
-    switch (instruction.toString().length) {
-        case 1:
-            currentInstruction = parseInt(instruction.toString().slice(0));
-            firstParameterMode = 0;
-            secondParameterMode = 0;
-            thirdParameterMode = 0;
-            break;
-        case 2:
-            currentInstruction = parseInt(instruction.toString().slice(0));
-            firstParameterMode = 0;
-            secondParameterMode = 0;
-            thirdParameterMode = 0;
-            break;
-        case 3:
-            currentInstruction = parseInt(instruction.toString().slice(1));
-            firstParameterMode = parseInt(instruction.toString()[0]);
-            secondParameterMode = 0;
-            thirdParameterMode = 0;
-            break;
-        case 4:
-            currentInstruction = parseInt(instruction.toString().slice(2));
-            firstParameterMode = parseInt(instruction.toString()[1]);
-            secondParameterMode = parseInt(instruction.toString()[0]);
-            thirdParameterMode = 0;
-            break;
-        case 5:
-            currentInstruction = parseInt(instruction.toString().slice(3));
-            firstParameterMode = parseInt(instruction.toString()[2]);
-            secondParameterMode = parseInt(instruction.toString()[1]);
-            thirdParameterMode = parseInt(instruction.toString()[0]);
-            break;
+class IntCodeComputer {
+    constructor(program) {
+        this.program = program;
+        this.inputInstruction = [];
+        this.output = [];
+        this.firstParameterMode;
+        this.secondParameterMode;
+        this.thirdParameterMode;
+        this.position = 0;
+        this.currentInstruction;
     }
-}
-
-function computeOperation (position, instruction) {
-    var firstParameter;
-    var secondParameter;
-    switch (firstParameterMode) {
-        case 0:
-            firstParameter = parseInt(diagnostic[diagnostic[position + 1]]);
-            break;
-        case 1:
-            firstParameter = parseInt([diagnostic[position + 1]]);
-            break;
+    
+    computeParameterModes (instruction) {
+        switch (instruction.toString().length) {
+            case 1:
+                this.currentInstruction = parseInt(instruction.toString().slice(0));
+                this.firstParameterMode = 0;
+                this.secondParameterMode = 0;
+                this.thirdParameterMode = 0;
+                break;
+            case 2:
+                this.currentInstruction = parseInt(instruction.toString().slice(0));
+                this.firstParameterMode = 0;
+                this.secondParameterMode = 0;
+                this.thirdParameterMode = 0;
+                break;
+            case 3:
+                this.currentInstruction = parseInt(instruction.toString().slice(1));
+                this.firstParameterMode = parseInt(instruction.toString()[0]);
+                this.secondParameterMode = 0;
+                this.thirdParameterMode = 0;
+                break;
+            case 4:
+                this.currentInstruction = parseInt(instruction.toString().slice(2));
+                this.firstParameterMode = parseInt(instruction.toString()[1]);
+                this.secondParameterMode = parseInt(instruction.toString()[0]);
+                this.thirdParameterMode = 0;
+                break;
+            case 5:
+                this.currentInstruction = parseInt(instruction.toString().slice(3));
+                this.firstParameterMode = parseInt(instruction.toString()[2]);
+                this.secondParameterMode = parseInt(instruction.toString()[1]);
+                this.thirdParameterMode = parseInt(instruction.toString()[0]);
+                break;
+        }
     }
-    switch (secondParameterMode) {
-        case 0:
-            secondParameter = parseInt(diagnostic[diagnostic[position + 2]]);
-            break;
-        case 1:
-            secondParameter = parseInt([diagnostic[position + 2]]);
-            break;
-    }
-
-    switch(instruction) {
-        case 99:
-            return -1;
-        case 1:
-            diagnostic[diagnostic[position + 3]] = firstParameter + secondParameter;
-            return (position + 4);
-        case 2:
-            diagnostic[diagnostic[position + 3]] = firstParameter * secondParameter;
-            return (position + 4);
-        case 3:
-            diagnostic[diagnostic[position + 1]] = inputInstruction.shift();
-            return (position + 2);
-        case 4:
-            output.push(firstParameter);
-            return (position + 2);
-        case 5:
-            if (firstParameter != 0) {
-                return secondParameter;
-            } 
-            else {
-                return (position + 3);
-            }
-            break;
-        case 6:
-            if (firstParameter == 0) {
-                return secondParameter;
-            }
-            else {
-                return (position + 3);
-            }
-            break;
-        case 7:
-            if (firstParameter < secondParameter) {
-                diagnostic[diagnostic[position + 3]] = 1;
-                return (position + 4);
-            }
-            else {
-                diagnostic[diagnostic[position + 3]] = 0;
-                return (position + 4);
-            }
-            break;
-        case 8:
-            if (firstParameter == secondParameter) {
-                diagnostic[diagnostic[position + 3]] = 1;
-                return (position + 4);
-            }
-            else {
-                diagnostic[diagnostic[position + 3]] = 0;
-                return (position + 4);
-            }
-            break;
+    
+    computeOperation (position, instruction) {
+        var firstParameter;
+        var secondParameter;
+        switch (this.firstParameterMode) {
+            case 0:
+                firstParameter = parseInt(this.program[this.program[this.position + 1]]);
+                break;
+            case 1:
+                firstParameter = parseInt(this.program[this.position + 1]);
+                break;
+        }
+        switch (this.secondParameterMode) {
+            case 0:
+                secondParameter = parseInt(this.program[this.program[this.position + 2]]);
+                break;
+            case 1:
+                secondParameter = parseInt(this.program[this.position + 2]);
+                break;
+        }
+    
+        switch(instruction) {
+            case 99:
+                return -1;
+            case 1:
+                this.program[this.program[this.position + 3]] = firstParameter + secondParameter;
+                return (this.position + 4);
+            case 2:
+                this.program[this.program[this.position + 3]] = firstParameter * secondParameter;
+                return (this.position + 4);
+            case 3:
+                this.program[this.program[this.position + 1]] = this.inputInstruction.shift();
+                return (position + 2);
+            case 4:
+                this.output.push(firstParameter);
+                return (this.position + 2);
+            case 5:
+                if (firstParameter != 0) {
+                    return secondParameter;
+                } 
+                else {
+                    return (this.position + 3);
+                }
+                break;
+            case 6:
+                if (firstParameter == 0) {
+                    return secondParameter;
+                }
+                else {
+                    return (this.position + 3);
+                }
+                break;
+            case 7:
+                if (firstParameter < secondParameter) {
+                    this.program[this.program[this.position + 3]] = 1;
+                    return (this.position + 4);
+                }
+                else {
+                    this.program[this.program[this.position + 3]] = 0;
+                    return (this.position + 4);
+                }
+                break;
+            case 8:
+                if (firstParameter == secondParameter) {
+                    this.program[this.program[this.position + 3]] = 1;
+                    return (this.position + 4);
+                }
+                else {
+                    this.program[this.program[this.position + 3]] = 0;
+                    return (this.position + 4);
+                }
+                break; 
+        }
     }
 }
 
@@ -123,16 +129,15 @@ function main() {
     var sequences = Combinatorics.permutation([0,1,2,3,4]).toArray();
     sequences.forEach(function (i) {
         var amplifierOutput = 0;
-        diagnostic = puzzleInput.slice(0);
         i.forEach(function (e) {
-            inputInstruction.push(e);
-            inputInstruction.push(amplifierOutput);
-            var position = 0;
-            while (position != -1) {
-                computeParameterModes(diagnostic[position]);
-                position = computeOperation(position, currentInstruction);
+            computer = new IntCodeComputer(puzzleInput);
+            computer.inputInstruction.push(e);
+            computer.inputInstruction.push(amplifierOutput);
+            while (computer.position != -1) {
+                computer.computeParameterModes(computer.program[computer.position]);
+                computer.position = computer.computeOperation(computer.position, computer.currentInstruction);
             }
-            amplifierOutput = output.pop();
+            amplifierOutput = computer.output.pop();
         });
         if (amplifierOutput > maxThrusterOutput) { 
             maxThrusterOutput = amplifierOutput;
