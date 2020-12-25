5509ad936b4eb50f4527284829f22956ebcd7ef2
Steven Bloch <snbloch@gmail.com>
2020-12-24 17:40:37 -0800

    Finished day22 part2.

--- 2020/snbloch/day22/solution.py
+++ 2020/snbloch/day22/solution.py
@@ -1,43 +1,85 @@
 from collections import deque
 
-class deck:
+class Deck:
     def __init__(self):
         self.cards = deque()
 
-class player:
+class Player:
     def __init__(self, id):
         self.id = id
-        self.deck = deck()
+        self.deck = Deck()
+        self.seen = set()
 
-class game:
-    def __init__(self):
+class Game:
+    def __init__(self, stateful):
+        self.finished = False
         self.players = []
-        with open('input.txt', 'r') as inputfile:
-            for p in inputfile.read().split('\n\n'):
-                self.player = player(int(p.strip().split('Player ')[1].split(':')[0]))
-                for i in p.strip().split(':')[1].strip().split('\n'):
-                    self.player.deck.cards.append(int(i))
-                self.players.append(self.player)
+        self.stateful = stateful
+        self.winner = None
+    def play_game(self):
+        while self.finished == False:
+            self.play_round()
+            for p in self.players:
+                if len(p.deck.cards) == 0:
+                    self.finished = True
     def play_round(self):
         round = {}
-        for p in self.players:
-            round[p.id] = p.deck.cards.popleft()
-        winner = max(round, key=round.get)
-        for p in self.players:
-            if p.id == winner:
-                p.deck.cards.append(round[p.id])
-                del round[p.id]
-                for r in round:
-                    p.deck.cards.append(round[r])
+        if self.stateful == True:
+            seen_before = True
+            for p in self.players:
+                if ''.join(str(p.deck.cards)) not in p.seen:
+                    seen_before = False
+                p.seen.add(''.join(str(p.deck.cards)))
+            if seen_before == True:
+                self.finished = True
+                self.winner = self.players[0].id
+            else:
+                play_recursive = True
+                for p in self.players:
+                    round[p.id] = p.deck.cards.popleft()
+                    if len(p.deck.cards) < round[p.id]:
+                        play_recursive = False
+                if play_recursive == True:
+                    game_recursive = Game(True)
+                    for p in self.players:
+                        recursive_deck = Deck()
+                        for i in list(p.deck.cards)[:round[p.id]]:
+                            recursive_deck.cards.append(i)
+                        p1 = Player(p.id)
+                        p1.deck = recursive_deck
+                        game_recursive.players.append(p1)
+                    game_recursive.play_game()
+                    self.winner = game_recursive.winner
+                else:
+                    self.winner = max(round, key=round.get)
+            for p in self.players:
+                if p.id == self.winner:
+                    if len(round) > 0:
+                        p.deck.cards.append(round[p.id])
+                        del round[p.id]
+                        for r in round:
+                            p.deck.cards.append(round[r])
+        else:
+            for p in self.players:
+                round[p.id] = p.deck.cards.popleft()
+            self.winner = max(round, key=round.get)
+            for p in self.players:
+                if p.id == self.winner:
+                    p.deck.cards.append(round[p.id])
+                    del round[p.id]
+                    for r in round:
+                        p.deck.cards.append(round[r])
 
 def part1():
-    cardgame = game()
-    finished = False
-    while finished != True:
-        cardgame.play_round()
-        for player in cardgame.players:
-            if len(player.deck.cards) == 0:
-                finished = True
+    cardgame = Game(False)
+    with open('input.txt', 'r') as inputfile:
+        players = inputfile.read().split('\n\n')
+    for p in players:
+        p1 = Player(int(p.strip().split('Player ')[1].split(':')[0]))
+        for i in p.strip().split(':')[1].strip().split('\n'):
+            p1.deck.cards.append(int(i))
+        cardgame.players.append(p1)
+    cardgame.play_game()
     for p in cardgame.players:
         if len(p.deck.cards) > 0:
             score = 0
@@ -45,6 +87,23 @@ def part1():
                 score += i * p.deck.cards.pop()
             print(score)
 
+def part2():
+    cardgame = Game(True)
+    with open('input.txt', 'r') as inputfile:
+        players = inputfile.read().split('\n\n')
+    for p in players:
+        p1 = Player(int(p.strip().split('Player ')[1].split(':')[0]))
+        for i in p.strip().split(':')[1].strip().split('\n'):
+            p1.deck.cards.append(int(i))
+        cardgame.players.append(p1)
+    cardgame.play_game()
+    for p in cardgame.players:
+        if p.id == cardgame.winner:
+            score = 0
+            for i in range(1, len(p.deck.cards) + 1):
+                score += i * p.deck.cards.pop()
+            print(score)
+
 if __name__ == '__main__':
     part1()
-
+    part2()
